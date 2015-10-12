95895f3d029f5903ee5ea8b3a8a3317e14a23fb1
Dave Brotherstone <davegb@pobox.com>
2015-10-12 19:22:39 +0200

    Remove unnecessary requires
    This was requiring `react/addons` which produces a warning in react
    0.14. We don't need it - it had been forgotten. Other requires were just
    taking up space.

--- src/unexpected-react-shallow.js
+++ src/unexpected-react-shallow.js
@@ -1,14 +1,6 @@
-var React = require('react/addons');
-var ArrayChanges = require('array-changes');
-
-var Element = require('./lib/element');
-var Write = require('./lib/write');
-var Equality = require('./lib/equality');
-var Diff = require('./lib/diff');
 var ReactElement = require('./lib/ReactElement');
 var ReactShallowRenderer = require('./lib/ReactShallowRenderer');
 var Assertions = require('./lib/assertions');
-
 
 module.exports = {
 
